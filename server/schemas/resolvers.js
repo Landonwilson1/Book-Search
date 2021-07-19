343df21941f0297a2a3dd918149a34f16017a7c7
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-07-19 12:38:05 -0400

    updated mutation and type defs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,13 +6,23 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-
+        const userData = await User.findOne({ _id: context.user._id })
+        .select('-__v -password').populate('bookSchema')
         return userData;
       }
 
       throw new AuthenticationError('Not logged in');
     },
+  },
+  users: async () => {
+    return User.find()
+      .select('-__v -password')
+      .populate('bookSchema');
+  },
+  user: async (parent, { username }) => {
+    return User.findOne({ username })
+      .select('-__v -password')
+      .populate('bookSchema')
   },
 
   Mutation: {
@@ -38,17 +48,15 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: async (parent, { bookData }, context) => {
+    saveBook: async (parent, args, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $push: { savedBooks: bookData } },
+          { $push: { savedBooks: args.input} },
           { new: true }
         );
-
         return updatedUser;
       }
-
       throw new AuthenticationError('You need to be logged in!');
     },
     removeBook: async (parent, { bookId }, context) => {
